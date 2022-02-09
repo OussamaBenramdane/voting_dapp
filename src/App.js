508fa663507a8cfe80f672f6f003927f573cbc29
Oussama Benramdane <mr.benramdane.oussama@gmail.com>
2022-02-09 23:36:00 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
 
 function App() {
+  // web3.setProvider('ws://localhost:8546');
+  const web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+<script src='https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js'></script>;
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
